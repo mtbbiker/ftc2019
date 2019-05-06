a5dd2bd54006fa838ef38029c05bddc0e624a9c3
eugenet <eugenet@netstar.co.za>
2019-05-06 08:49:34 +0200

    Training update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUtest.java
@@ -21,6 +21,10 @@ public class IMUtest extends LinearOpMode
     private Orientation angles;
     private Acceleration gravity;
 
+    Orientation             lastAngles = new Orientation();
+    double                  globalAngle, power = .30, correction;
+
+
     @Override
     public void runOpMode()
     {
@@ -36,9 +40,39 @@ public class IMUtest extends LinearOpMode
 
         imu.initialize(parameters);
 
-
+//        byte AXIS_MAP_CONFIG_BYTE = 0x6; //This is what to write to the AXIS_MAP_CONFIG register to swap x and z axes
+//        byte AXIS_MAP_SIGN_BYTE = 0x1; //This is what to write to the AXIS_MAP_SIGN register to negate the z axis
+//
+//        //Need to be in CONFIG mode to write to registers
+//        imu.write8(BNO055IMU.Register.OPR_MODE,BNO055IMU.SensorMode.CONFIG.bVal & 0x0F);
+//
+//        sleep(100); //Changing modes requires a delay before doing anything else
+//
+//        //Write to the AXIS_MAP_CONFIG register
+//        imu.write8(BNO055IMU.Register.AXIS_MAP_CONFIG,AXIS_MAP_CONFIG_BYTE & 0x0F);
+//
+//        //Write to the AXIS_MAP_SIGN register
+//        imu.write8(BNO055IMU.Register.AXIS_MAP_SIGN,AXIS_MAP_SIGN_BYTE & 0x0F);
+//
+//        //Need to change back into the IMU mode to use the gyro
+//        imu.write8(BNO055IMU.Register.OPR_MODE,BNO055IMU.SensorMode.IMU.bVal & 0x0F);
+//
+//        sleep(100); //Changing modes again requires a delay
 
         telemetry.setMsTransmissionInterval(100);
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!isStopRequested() && !imu.isGyroCalibrated())
+        {
+            sleep(50);
+            idle();
+        }
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        telemetry.update();
+
+        // wait for start button.
         waitForStart();
 
         while (opModeIsActive())
@@ -56,6 +90,7 @@ public class IMUtest extends LinearOpMode
         telemetry.addData("Heading", formatAngle(angles.angleUnit, angles.firstAngle));
         telemetry.addData("Roll", formatAngle(angles.angleUnit, angles.secondAngle));
         telemetry.addData("Pitch", formatAngle(angles.angleUnit, angles.thirdAngle));
+
         telemetry.addData("Grav", gravity.toString());
         telemetry.update();
     }
@@ -69,4 +104,65 @@ public class IMUtest extends LinearOpMode
     {
         return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
+
+    /**
+     * Resets the cumulative angle tracking to zero.
+     */
+    private void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+
+    /**
+     * Get current cumulative angle rotation from last reset.
+     * @return Angle in degrees. + = left, - = right.
+     */
+    private double getAngle()
+    {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
+
+    /**
+     * See if we are moving in a straight line and if not return a power correction value.
+     * @return Power adjustment, + is adjust left - is adjust right.
+     */
+    private double checkDirection()
+    {
+        // The gain value determines how sensitive the correction is to direction changes.
+        // You will have to experiment with your robot to get small smooth direction changes
+        // to stay on a straight line.
+        double correction, angle, gain = .10;
+
+        angle = getAngle();
+
+        if (angle == 0)
+            correction = 0;             // no adjustment.
+        else
+            correction = -angle;        // reverse sign of angle for correction.
+
+        correction = correction * gain;
+
+        return correction;
+    }
+
 }
